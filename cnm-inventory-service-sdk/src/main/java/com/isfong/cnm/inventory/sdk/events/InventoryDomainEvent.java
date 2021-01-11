7128035c9767735fc77f954b7485ef6c8f761d10
isfong <isfong@qq.com>
2021-01-11 14:10:47 +0800

    Fix InventoryDomainEvent.aggregateType

--- cnm-inventory-service-sdk/src/main/java/com/isfong/cnm/inventory/sdk/events/InventoryDomainEvent.java
+++ cnm-inventory-service-sdk/src/main/java/com/isfong/cnm/inventory/sdk/events/InventoryDomainEvent.java
@@ -3,5 +3,5 @@ package com.isfong.cnm.inventory.sdk.events;
 import io.eventuate.tram.events.common.DomainEvent;
 
 public abstract class InventoryDomainEvent implements DomainEvent {
-    public static final String aggregateType = "com.example.inventory.domain.models.entities.Inventory";
+    public static final String aggregateType = "com.isfong.cnm.inventory.domain.models.entities.Inventory";
 }
