e4dc48edc5e034ef7ca2ea02247f422bd35a7d03
isfong <isfong@qq.com>
2021-01-23 09:53:08 +0800

    update cors.sh

--- build-dependency-docker-images/cors-swagger-ui/cors.sh
+++ build-dependency-docker-images/cors-swagger-ui/cors.sh
@@ -13,12 +13,32 @@ for corsUrl in $corsUrls; do
         fi
         let index++
     done
-    sed -i "${line}i $location" /etc/nginx/nginx.conf
-    let line++
-    sed -i "${line}i \\      proxy_pass ${pathArray[0]}//${pathArray[1]};" /etc/nginx/nginx.conf
-    let line++
-    sed -i "${line}i \\    }" /etc/nginx/nginx.conf
-    let line++
+
+    existProxy=$(cat /etc/nginx/nginx.conf | grep "$location")
+    if [[ $existProxy ]]; then
+      echo "existed -> $location" >> /cors-existed.txt
+      echo "existed -> \\      proxy_pass ${pathArray[0]}//${pathArray[1]};" >> /cors-existed.txt
+      echo "existed -> \\    }" >> /cors-existed.txt
+    else
+      sed -i "${line}i $location" /etc/nginx/nginx.conf
+      echo "${line}i $location" >> /cors-configured.txt
+      let line++
+      sed -i "${line}i \\      proxy_pass ${pathArray[0]}//${pathArray[1]};" /etc/nginx/nginx.conf
+      echo "${line}i \\      proxy_pass ${pathArray[0]}//${pathArray[1]};" >> /cors-configured.txt
+      let line++
+      sed -i "${line}i \\    }" /etc/nginx/nginx.conf
+      echo "${line}i \\    }" >> /cors-configured.txt
+      let line++
+    fi
 done
 
-echo 'cors ran' >> /cors-ran.txt
+if [[ $ACCESS_URL ]]; then
+  accessUrlConfigured=$(cat $SWAGGER_JSON | grep "$ACCESS_URL")
+  if [[ $accessUrlConfigured ]]; then
+    echo "$accessUrlConfigured existed in $SWAGGER_JSON" >> /skip-conf-access-url.txt
+  else
+    sed -i "s#- url:.*#- url: '$ACCESS_URL'#" $SWAGGER_JSON
+    echo "conf $ACCESS_URL to $SWAGGER_JSON" >> /access-url-configured.txt
+  fi
+fi
+
